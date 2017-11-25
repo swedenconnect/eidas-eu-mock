cb53b5f3653a8cc96d3bd0b16410366cfa0f6808
Stefan Santesson <stefan@aaa-sec.com>
2017-11-26 00:31:01 +0100

    Cleanup

--- cef-node-docker/src/main/scripts/dockerStart.sh
+++ cef-node-docker/src/main/scripts/dockerStart.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-#if [ $# -eq 1 ]; then
-#    myapp=$1
-#else
-#    echo "$0 Need name of app as argument"
-#    exit 1
-#fi
-#
-## Source this app's config
-##
-#for i in `\ls /etc/${myapp}/env/*.conf 2>/dev/null`
-#do
-#    echo sourcing $i
-#    source $i
-#done
-
 TOMCAT_HOME=/opt/tomcat
-
 
 #
 # Environment variable settings
@@ -28,7 +12,6 @@ TOMCAT_HOME=/opt/tomcat
 : ${IDP_CONFIG_REPOSITORY:=/opt/docker/internal/configEidas/CA/idp/}
 : ${EIDAS_TRUSTED_CERTS_FILE:=/opt/docker/internal/configEidas/trust/trustedCerts.pem}
 : ${EIDAS_TRUSTED_CERTS_CONSTRAINTS:=eidasKeyStore_Connector_CA.jks,eidasKeyStore_Service_CA.jks}
-
 
 export EIDAS_CONFIG_REPOSITORY=$EIDAS_CONFIG_REPOSITORY
 export SPECIFIC_CONFIG_REPOSITORY=$SPECIFIC_CONFIG_REPOSITORY
@@ -42,9 +25,6 @@ export EIDAS_TRUSTED_CERTS_CONSTRAINTS=$EIDAS_TRUSTED_CERTS_CONSTRAINTS
 #
 : ${JVM_MAX_HEAP:=1536m}
 : ${JVM_START_HEAP:=512m}
-#: ${JMX_PORT:=9152}
-#: ${JMX_ACCESS_FILE:=/etc/common-config/jmxremote.access}
-#: ${JMX_PASSWORD_FILE:=/etc/common-config/jmxremote.password}
 
 export JAVA_OPTS="-XX:MaxPermSize=512m"
 export CATALINA_OPTS="\
