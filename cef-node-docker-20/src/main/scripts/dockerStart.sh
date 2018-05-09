042f9680d8d6ac27a1017c6afad149d4035182b8
Stefan Santesson <stefan@aaa-sec.com>
2018-05-09 16:12:36 +0200

    CEF 2.0 docker version

---
+++ cef-node-docker-20/src/main/scripts/dockerStart.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+TOMCAT_HOME=/opt/tomcat
+
+#
+# This is the docker run script that is placed in the $CATALINA_HOME/bin folder to be executed inside a docker container
+#
+
+: ${EIDAS_CONFIG_REPOSITORY:=/opt/tomcat/configEidas/XA/}
+: ${SPECIFIC_CONFIG_REPOSITORY:=/opt/tomcat/configEidas/XA/specific/}
+: ${SP_CONFIG_REPOSITORY:=/opt/tomcat/configEidas/XA/sp/}
+: ${IDP_CONFIG_REPOSITORY:=/opt/tomcat/configEidas/XA/idp/}
+: ${EIDAS_TRUSTED_CERTS_FILE:=/opt/tomcat/configEidas/trust/trustedCerts.pem}
+: ${EIDAS_TRUSTED_CERTS_CONSTRAINTS:=eidasKeyStore_XA.jks}
+
+export EIDAS_CONFIG_REPOSITORY=$EIDAS_CONFIG_REPOSITORY
+export SPECIFIC_CONFIG_REPOSITORY=$SPECIFIC_CONFIG_REPOSITORY
+export SP_CONFIG_REPOSITORY=$SP_CONFIG_REPOSITORY
+export IDP_CONFIG_REPOSITORY=$IDP_CONFIG_REPOSITORY
+export EIDAS_TRUSTED_CERTS_FILE=$EIDAS_TRUSTED_CERTS_FILE
+export EIDAS_TRUSTED_CERTS_CONSTRAINTS=$EIDAS_TRUSTED_CERTS_CONSTRAINTS
+
+#
+# System settings
+#
+: ${JVM_MAX_HEAP:=1536m}
+: ${JVM_START_HEAP:=512m}
+
+export JAVA_OPTS="-XX:MaxPermSize=512m"
+export CATALINA_OPTS="\
+          -Xmx${JVM_MAX_HEAP}\
+          -Xms${JVM_START_HEAP}\
+"
+
+${TOMCAT_HOME}/bin/catalina.sh run
