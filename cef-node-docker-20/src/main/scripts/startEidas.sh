371d8c0631a7075ac6ae57e06b5b284b6b382e45
Stefan Santesson <stefan@aaa-sec.com>
2018-05-10 14:35:38 +0200

    working version

--- cef-node-docker-20/src/main/scripts/startEidas.sh
+++ cef-node-docker-20/src/main/scripts/startEidas.sh
@@ -7,9 +7,11 @@
 usage() {
     echo "Usage: $0 [options...]" >&2
     echo
-    echo "   -d, --confdir          Configuration directory (default is \$CATALINA_HOME/configEidas)"
+    echo "   -c, --confdir          Configuration directory (default is \$CATALINA_HOME/configEidas)"
+    echo "   -d                     Debug mode"
     echo "   -f, --folder           Configuration folder (default is XA)"
     echo "   -h, --help             Prints this help"
+    echo "   -t, --contraints       Set the trusted certs constraints (Comma separated line of key store path endings)"
     echo
 }
 
@@ -28,6 +30,8 @@ CATALINA_HOME=$TOMCAT_HOME
 
 BASE_CONFIG_PATH=$CATALINA_HOME/configEidas
 CONFIG_FOLDER=XA
+TRUSTED_CERTS_CONSTRAINTS=
+DEBUG_MODE=false
 
 while :
 do
@@ -36,7 +40,7 @@ do
 	    usage
 	    exit 0
 	    ;;
-	-d | --confdir)
+	-c | --confdir)
 	    BASE_CONFIG_PATH="$2"
 	    shift 2
 	    ;;
@@ -44,7 +48,16 @@ do
 	    CONFIG_FOLDER="$2"
 	    shift 2
 	    ;;
+	-t | --constraints)
+	    TRUSTED_CERTS_CONSTRAINTS="$2"
+	    shift 2
+	    ;;
 	--)
+	    shift
+	    break;
+	    ;;
+	-d)
+        DEBUG_MODE=true
 	    shift
 	    break;
 	    ;;
@@ -62,11 +75,12 @@ done
 
 # ENV variables
 export EIDAS_CONFIG_REPOSITORY=${BASE_CONFIG_PATH}/${CONFIG_FOLDER}/
-export SPECIFIC_CONFIG_REPOSITORY=${BASE_CONFIG_PATH}/${CONFIG_FOLDER}/specific/
+export SPECIFIC_CONNECTOR_CONFIG_REPOSITORY=${BASE_CONFIG_PATH}/${CONFIG_FOLDER}/specificConnector/
+export SPECIFIC_PROXY_SERVICE_CONFIG_REPOSITORY=${BASE_CONFIG_PATH}/${CONFIG_FOLDER}/specificProxyService/
 export SP_CONFIG_REPOSITORY=${BASE_CONFIG_PATH}/${CONFIG_FOLDER}/sp/
 export IDP_CONFIG_REPOSITORY=${BASE_CONFIG_PATH}/${CONFIG_FOLDER}/idp/
 export EIDAS_TRUSTED_CERTS_FILE=${BASE_CONFIG_PATH}/trust/trustedCerts.pem
-export EIDAS_TRUSTED_CERTS_CONSTRAINTS=eidasKeyStore_Connector_CA.jks,eidasKeyStore_Service_CA.jks
+export EIDAS_TRUSTED_CERTS_CONSTRAINTS=${TRUSTED_CERTS_CONSTRAINTS}
 
 #
 # System settings
@@ -80,10 +94,11 @@ export CATALINA_OPTS="-Xms512m -Xmx1536m"
 export JPDA_ADDRESS=8000
 export JPDA_TRANSPORT=dt_socket
 
-if [ "$1" == "-d" ]; then
+if [ $DEBUG_MODE == true ]; then
     echo "Running in debug"    
     $CATALINA_HOME/bin/catalina.sh jpda run
 else
+    echo "Running in normal mode"
     $CATALINA_HOME/bin/catalina.sh run
 fi
 
