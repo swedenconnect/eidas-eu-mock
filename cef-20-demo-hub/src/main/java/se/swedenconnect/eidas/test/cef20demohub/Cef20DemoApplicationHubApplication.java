29d3712857f2bef91a9da6e6e335136e809f9814
Stefan Santesson <stefan@aaa-sec.com>
2018-05-11 16:39:11 +0200

    Basic project created

---
+++ cef-20-demo-hub/src/main/java/se/swedenconnect/eidas/test/cef20demohub/Cef20DemoApplicationHubApplication.java
@@ -0,0 +1,12 @@
+package se.swedenconnect.eidas.test.cef20demohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cef20DemoApplicationHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cef20DemoApplicationHubApplication.class, args);
+    }
+}
---
+++ cef-20-demo-hub/src/test/java/se/swedenconnect/eidas/test/cef20demohub/Cef20DemoApplicationHubApplicationTests.java
@@ -0,0 +1,16 @@
+package se.swedenconnect.eidas.test.cef20demohub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cef20DemoApplicationHubApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
