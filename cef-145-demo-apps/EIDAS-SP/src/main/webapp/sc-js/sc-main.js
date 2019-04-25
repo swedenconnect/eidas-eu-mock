032adfce469b1ab938667b2211128c4a5a5c1c11
Stefan Santesson <stefan@aaa-sec.com>
2019-04-25 16:03:38 +0200

    fixed requested attr radio btn

--- cef-145-demo-apps/EIDAS-SP/src/main/webapp/sc-js/sc-main.js
+++ cef-145-demo-apps/EIDAS-SP/src/main/webapp/sc-js/sc-main.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 		$('.ns-providers').toggleClass('hide');
 	});
 
-	processAttrs("reqNpAttr");
-	processAttrs("reqLpAttr");
+	processAttrsRd("reqNpAttr");
+	processAttrsRd("reqLpAttr");
 
 });
 
@@ -37,3 +37,35 @@ function processAttrs(prefix){
 		}
 	});
 }
+
+function processAttrsRd(prefix){
+	$("input[name^='"+prefix+"']:checked").each(function (index) {
+		var selectedItem = $(this).val();
+		var td = $(this).closest("td").find("div").eq(0).find("div").eq(0);
+		switch (selectedItem) {
+			case "n":
+				td.attr("class", "col-lg-8 col-sm-12 attr-no-req");
+				break;
+			case "o":
+				td.attr("class", "col-lg-8 col-sm-12 attr-request");
+				break;
+			case "r":
+				td.attr("class", "col-lg-8 col-sm-12 attr-require");
+				break;
+		}
+	});
+}
+
+function selectAllAttr(prefix, opt) {
+	$("input[name^='"+prefix+"']").each(function (index) {
+		var optionVal = $(this).val();
+		if (optionVal === opt){
+			$(this).prop("checked", true);
+		} else {
+			$(this).prop("checked", false);
+		}
+	});
+	processAttrsRd(prefix);
+}
+
+
