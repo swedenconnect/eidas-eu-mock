ecda205589f2c1395a92703cc93d98dbbd99b9f5
Stefan Santesson <stefan@aaa-sec.com>
2019-08-13 10:24:46 +0200

    CEF 2.3.1 updates

--- cef-node-docker-230/buildwars.sh
+++ cef-node-docker-230/buildwars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo ******* building eidas node war dependencies *********
-mvn -f ../eIDASNodeDev-2.3.0/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite
+mvn -f ../eIDASNodeDev-2.3.1/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite
 
