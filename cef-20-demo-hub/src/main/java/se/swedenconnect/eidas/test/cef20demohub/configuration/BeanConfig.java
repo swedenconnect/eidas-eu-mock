f138059d3c2caa078d23e598f03126c2fa17d09e
Stefan Santesson <stefan@aaa-sec.com>
2022-06-22 17:54:58 +0200

    cef 20 hub

--- cef-20-demo-hub/src/main/java/se/swedenconnect/eidas/test/cef20demohub/configuration/BeanConfig.java
+++ cef-20-demo-hub/src/main/java/se/swedenconnect/eidas/test/cef20demohub/configuration/BeanConfig.java
@@ -4,20 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 public class BeanConfig {
-
-    @Bean
-    public CookieSerializer cookieSerializer() {
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-        serializer.setCookieName("JSESSIONID.HUB");
-        serializer.setCookiePath("/");
-        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
-        return serializer;
-    }
 
     @Bean
     public ObjectMapper yamlObjectMapper () {
