9d526262680badb08cc9ede45f13e6015e4e7859
Stefan Santesson <stefan@aaa-sec.com>
2022-11-01 23:14:18 +0100

    updating build strategy

--- cef-node-docker-260/buildwars.sh
+++ cef-node-docker-260/buildwars.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 echo ******* building eidas node war dependencies *********
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite
+mvn -f ../md-trust/pom.xml clean install
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-SpecificCommunicationDefinition clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
 
