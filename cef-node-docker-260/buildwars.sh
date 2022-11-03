4d3ba5bb20c64186ba270e9c8439982d2662af77
Stefan Santesson <stefan@aaa-sec.com>
2022-11-03 20:09:48 +0100

    Update buildwars.sh

--- cef-node-docker-260/buildwars.sh
+++ cef-node-docker-260/buildwars.sh
@@ -4,29 +4,23 @@ echo ******* building eidas node war dependencies *********
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-modules=(
-  "EIDAS-Light-Commons" \
-  "EIDAS-Commons" \
-  "EIDAS-Encryption" \
-  "EIDAS-Metadata" \
-  "EIDAS-JCache-Dev" \
-  "EIDAS-JCache-Ignite" \
-  "EIDAS-JCache-Ignite-Specific-Communication" \
-  "EIDAS-SpecificCommunicationDefinition")
+modules=("EIDAS-Light-Commons" "EIDAS-Commons" "EIDAS-Encryption" "EIDAS-Metadata" "EIDAS-JCache-Dev" "EIDAS-JCache-Ignite" "EIDAS-JCache-Ignite-Specific-Communication" "EIDAS-SpecificCommunicationDefinition")
 
 cd ../md-trust
 mvn clean install
-cd $DIR
+cd ../cef-node-docker-260
 
 for module in "${modules[@]}"; do
-  cd ../EIDAS-SOURCES-2.6.0-MDSL/$module
+  echo "Building cef module $module"
+  cd ../EIDAS-Sources-2.6.0-MDSL/$module
   mvn clean install -Dmaven.test.skip=true
-  cd $DIR
+  cd ../../cef-node-docker-260
 done
 
-cd ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent
+cd ../EIDAS-Sources-2.6.0-MDSL/EIDAS-Parent
 mvn clean install -Dmaven.test.skip=true
 mvn clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
+cd ../../cef-node-docker-260
 
 
 
