959f994f5afcd6e13b1c27347006d85cf93f6123
Stefan Santesson <stefan@aaa-sec.com>
2022-11-03 19:07:48 +0100

    Update buildwars.sh

--- cef-node-docker-260/buildwars.sh
+++ cef-node-docker-260/buildwars.sh
@@ -2,6 +2,13 @@
 
 echo ******* building eidas node war dependencies *********
 mvn -f ../md-trust/pom.xml clean install
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Light-Commons clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Commons clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Encryption clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Metadata clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Dev clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Ignite clean install -Dmaven.test.skip=true
+mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Ignite-Specific-Communication clean install -Dmaven.test.skip=true
 mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-SpecificCommunicationDefinition clean install -Dmaven.test.skip=true
 mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
 
