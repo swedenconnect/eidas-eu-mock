d0dbeee89e538174e51153ac007d527b0c09ba51
Stefan Santesson <stefan@aaa-sec.com>
2022-11-03 19:33:44 +0100

    Update buildwars.sh

--- cef-node-docker-260/buildwars.sh
+++ cef-node-docker-260/buildwars.sh
@@ -1,14 +1,43 @@
 #!/usr/bin/env bash
 
 echo ******* building eidas node war dependencies *********
-mvn -f ../md-trust/pom.xml clean install
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Light-Commons clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Commons clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Encryption clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Metadata clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Dev clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Ignite clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Ignite-Specific-Communication clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-SpecificCommunicationDefinition clean install -Dmaven.test.skip=true
-mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+modules=(
+  "EIDAS-Light-Commons" \
+  "EIDAS-Commons" \
+  "EIDAS-Encryption" \
+  "EIDAS-Metadata" \
+  "EIDAS-JCache-Dev" \
+  "EIDAS-JCache-Ignite" \
+  "EIDAS-JCache-Ignite-Specific-Communication" \
+  "EIDAS-SpecificCommunicationDefinition")
+
+cd ../md-trust
+mvn clean install
+cd $DIR
+
+for module in "${modules[@]}"; do
+  cd ../EIDAS-SOURCES-2.6.0-MDSL/$module
+  mvn clean install -Dmaven.test.skip=true
+  cd $DIR
+done
+
+cd ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent
+mvn clean install -Dmaven.test.skip=true
+mvn clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
+
+
+
+#mvn -f ../md-trust/pom.xml clean install
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Light-Commons clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Commons clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Encryption clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Metadata clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Dev clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Ignite clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-JCache-Ignite-Specific-Communication clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-SpecificCommunicationDefinition clean install -Dmaven.test.skip=true
+#mvn -f ../EIDAS-SOURCES-2.6.0-MDSL/EIDAS-Parent/pom.xml clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
 
