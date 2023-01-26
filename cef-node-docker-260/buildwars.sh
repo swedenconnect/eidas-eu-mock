063f18dd538cecef1d4861f7d102697657aae8fb
Stefan Santesson <stefan@aaa-sec.com>
2023-01-26 10:20:26 +0100

    Update buildwars.sh

--- cef-node-docker-260/buildwars.sh
+++ cef-node-docker-260/buildwars.sh
@@ -2,23 +2,11 @@
 
 echo ******* building eidas node war dependencies *********
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-modules=("EIDAS-Light-Commons" "EIDAS-Commons" "EIDAS-Encryption" "EIDAS-Metadata" "EIDAS-JCache-Dev" "EIDAS-JCache-Ignite" "EIDAS-JCache-Ignite-Specific-Communication" "EIDAS-SpecificCommunicationDefinition")
-
-cd ../md-trust
+cd ../md-trust || exit
 mvn clean install
-cd ../cef-node-docker-260
 
-for module in "${modules[@]}"; do
-  echo "Building cef module $module"
-  cd ../EIDAS-Sources-2.6.0-MDSL/$module
-  mvn clean install -Dmaven.test.skip=true
-  cd ../../cef-node-docker-260
-done
+cd ../EIDAS-Sources-2.6.0-MDSL || exit
+mvn clean install -f EIDAS-Parent/pom.xml -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
 
-cd ../EIDAS-Sources-2.6.0-MDSL/EIDAS-Parent
-mvn clean install -Dmaven.test.skip=true
-mvn clean install -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
-cd ../../cef-node-docker-260
+cd ../cef-node-docker-260 || exit
 
