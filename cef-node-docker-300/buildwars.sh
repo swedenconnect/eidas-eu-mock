b5f176fb1e64e8a228c6db85230276814cd3fd48
Stefan Santesson <stefan@aaa-sec.com>
2025-12-29 23:19:41 +0100

    Updates to docker 3.0.0 and mdsl

--- cef-node-docker-300/buildwars.sh
+++ cef-node-docker-300/buildwars.sh
@@ -6,7 +6,7 @@ cd ../md-trust || exit
 mvn clean install
 
 cd ../EIDAS-Sources-3.0.0-MDSL || exit
-mvn clean install -f EIDAS-Parent/pom.xml -P NodeOnly,DemoToolsOnly,nodeJcacheIgnite,specificCommunicationJcacheIgnite -Dmaven.test.skip=true
+mvn clean install -f EIDAS-Parent/pom.xml -P NodeOnly,DemoToolsOnly,nodeJcacheHazelcast,specificCommunicationJcacheHazelcast -Dmaven.test.skip=true
 
 cd ../cef-node-docker-300 || exit
 
