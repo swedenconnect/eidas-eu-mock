fc018c0716a5febb79b28777a340d87742a6189f
Stefan Santesson <stefan@aaa-sec.com>
2025-12-31 12:01:14 +0100

    Setting NamIDPolicy to Transient in IdP if not set in SAML request

--- cef-20-demo-hub/src/main/java/se/swedenconnect/eidas/test/cef20demohub/process/AuthnResponseGenerator.java
+++ cef-20-demo-hub/src/main/java/se/swedenconnect/eidas/test/cef20demohub/process/AuthnResponseGenerator.java
@@ -75,7 +75,9 @@ public class AuthnResponseGenerator {
 
   private void setNameIdAndSubject(ResponseData responseData, AuthenticationRequest authenticationRequest) {
     List<Attribute> attributeList = responseData.getResponse().getAttributes();
-    String nameIdPolicy = authenticationRequest.getNameIdPolicy();
+    String nameIdPolicy = authenticationRequest.getNameIdPolicy() == null
+        ? "transient"
+        : authenticationRequest.getNameIdPolicy();
     responseData.getResponse().setNameId(nameIdPolicy);
 
     Optional<Attribute> natId = attributeList.stream()
